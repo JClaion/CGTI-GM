cd1b33bc1976a49662598476d48bbd30041642ca
JClaion <Joseclaionmartins@gmail.com>
2024-03-15 16:54:06 -0400

    initial commit

---
+++ CGTI-MS/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.tsx',
+            ssr: 'resources/js/ssr.tsx',
+            refresh: true,
+        }),
+        react(),
+    ],
+    watch: {
+        usePolling: true,
+        origin: 'htpp://localhost'
+    },
+    server: {
+        hmr:{
+            host: 'localhost'
+        }
+    }
+});
